3b8e1c5f67587a90e7abc9024256f0fb6f842cdb
Isaque Vitor <isaquevitor99@gmail.com>
2025-07-27 22:14:20 -0300

    Add files via upload

---
+++ Aritimeticos.py
@@ -0,0 +1,5 @@
+n = int(input("diga um numero"))
+print("O seu numero e {}, o cara que vem antes e {}, e o cara que vem depois e {}".format(n,n-1,n+1))
+N = int(input("diga-me outro numero? "))
+print("O dobro do numero {}, e o {}, agora o triplo e {}".format(N, N*2,N*3))
+input("Enter pra sair.")
